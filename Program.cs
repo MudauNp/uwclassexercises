049ea78f066eeb9491d18c9d4406ad2b678d0fe3
MudauNP <5100154@mypillar7uwc.ac.za>
2023-11-28 12:27:40 +0200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace FirstClassExercise
+{
+    internal class Program
+    {
+         public static string ReversedString(string forwardText)
+         {
+            string returnString = "";
+
+            for (int index = forwardText.Length - 1; index >=0; index--)
+            {
+                returnString += forwardText[index];
+            }
+              return returnString;
+            
+
+         }
+        static void Main(string[] args)
+            {
+               string input = Console.ReadLine();
+               string output = ReversedString(input);
+
+            if (input == output)
+                Console.WriteLine("True");
+            else Console.WriteLine("false");
+        }
+
+         
+
+       
+            
+        
+    }
+}
